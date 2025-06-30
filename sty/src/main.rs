aa07cdb3837afc2aae7a28f83b0cf34031fa69d8
Stijn van Dongen <micans@users.noreply.github.com>
2025-06-30 23:32:12 +0100

    some print and expect

--- sty/src/main.rs
+++ sty/src/main.rs
@@ -2,6 +2,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::any::type_name_of_val;
 
 use std::env;
 
@@ -15,11 +16,43 @@ fn main() {
     let colid: usize = args[1].parse().expect("Failed to parse string to integer");
     let thefile = &args[2];
 
-    if let Ok(lines) = read_lines(thefile) {
+    if let Ok(mut lines) = read_lines(thefile) {
+
+        let myline = lines.next();
+        println!("myline {}", type_name_of_val(&myline));
+
+        let val = myline.expect("Header line");
+        println!("val {}", type_name_of_val(&val));
+
+        let val2 = val.expect("oops not Ok 2");
+        println!("val2 {}", type_name_of_val(&val2));
+
+
+
+        let v2 = lines.next().expect("second line").expect("second value");
+        println!("v2 {}", type_name_of_val(&v2));
+        println!("result {}", v2);
+
+        // let val3 = val.expect("oops not Ok 3");
+        // println!("val3 {}", type_name_of_val(&val3));
+
+        // if let Some(h) = myline {
+        //   match h {
+        //     Ok(v) => {
+        //       println!("result is {}", v);
+        //       println!("{}", type_name_of_val(&v));
+        //     }
+        //     Err(e) => {
+        //       println!("error: {}", e);
+        //       std::process::exit(1)
+        //     }
+        //   }
+        // }
+
         // Consumes the iterator, returns an (Optional) String
         for line in lines.map_while(Result::ok) {
             let fields: Vec<&str> = line.split('\t').collect();
-            println!("{}", fields[colid]);
+            println!("Column value is {}", fields[colid]);
         }
     }
 }
